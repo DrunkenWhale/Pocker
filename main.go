5b55e4391782e9f9901d11e1044fbd2597ef8142
Pigeon377 <892197240@qq.com>
2022-06-24 20:51:40 +0800

    feat: two process start a new termnial

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 func main() {
+	fmt.Println(os.Args)
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("have not defined")
 	}
@@ -16,9 +21,33 @@ func main() {
 }
 
 func run() {
-	cmd := exec.Command(os.Args[2])
+	cmd := exec.Command(os.Args[0], append([]string{"child"}, os.Args[2])...)
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		// this filed on be supported in linux
+
+		//Cloneflags: syscall.CLONE_NEWUTS|syscall.CLONE_NEWPID,
+
+	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Run()
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func child() {
+	cmd := exec.Command(os.Args[2])
+	syscall.Sethostname([]byte("container"))
+	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
+	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		panic(err)
+	}
+	syscall.Unmount("/proc", 0)
 }
---
+++ makefile
@@ -0,0 +1,17 @@
+CMD=go
+BIN_PATH=bin
+SRC_PATH=src
+
+all: clean build install
+
+build:
+	${CMD} build -o ${BIN_PATH}/docker ${SRC_PATH}/*
+
+install:
+		cp bin/docker /usr/bin/docker
+		cp bin/docker /usr/local/bin/docker
+uninstall:
+		rm -rf /usr/bin/docker /usr/local/bin/dcoker
+		rm -rf bin/docker
+
+clear: uninstall
