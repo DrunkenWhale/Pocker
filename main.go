46f46a727e08cd81a0c8aff8bff367a952fed190
Pigeon377 <892197240@qq.com>
2022-06-24 23:20:32 +0800

    feat: change some detail

--- main.go
+++ main.go
@@ -11,22 +11,21 @@ func main() {
 	fmt.Println(os.Args)
 	switch os.Args[1] {
 	case "run":
-		run()
-	case "child":
-		child()
+		Run()
+	case "init":
+		Init()
 	default:
 		panic("have not defined")
 	}
 
 }
 
-func run() {
-	cmd := exec.Command(os.Args[0], append([]string{"child"}, os.Args[2])...)
+func Run() {
+	cmd := exec.Command(os.Args[0], append([]string{"init"}, os.Args[2])...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		// this filed on be supported in linux
 
-		//Cloneflags: syscall.CLONE_NEWUTS|syscall.CLONE_NEWPID,
-
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -37,15 +36,10 @@ func run() {
 	}
 }
 
-func child() {
-	cmd := exec.Command(os.Args[2])
+func Init() {
 	syscall.Sethostname([]byte("container"))
-	defaultMountFlags := syscall.MS_NOEXEC | syscall.MS_NOSUID | syscall.MS_NODEV
-	syscall.Mount("proc", "/proc", "proc", uintptr(defaultMountFlags), "")
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	syscall.Mount("proc", "/proc", "proc", 0, "")
+	err := syscall.Exec(os.Args[2], os.Args[2:], os.Environ())
 	if err != nil {
 		panic(err)
 	}
