d0483bce9a2139fc291a951a4f0f1f9cfad402cf
Pigeon377 <892197240@qq.com>
2022-06-25 09:34:47 +0800

    feat: new code struct

--- main.go
+++ main.go
@@ -1,31 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
 func main() {
-	fmt.Println(os.Args)
-	switch os.Args[1] {
-	case "run":
-		Run()
-	case "init":
-		Init()
-	default:
-		panic("have not defined")
-	}
-
-}
-
-func Run() {
-	cmd := exec.Command(os.Args[0], append([]string{"init"}, os.Args[2])...)
+	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		// this filed on be supported in linux
-
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWUTS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -34,14 +18,4 @@ func Run() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func Init() {
-	syscall.Sethostname([]byte("container"))
-	syscall.Mount("proc", "/proc", "proc", 0, "")
-	err := syscall.Exec(os.Args[2], os.Args[2:], os.Environ())
-	if err != nil {
-		panic(err)
-	}
-	syscall.Unmount("/proc", 0)
 }
